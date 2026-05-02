7cf5d44d66bd1124a55151cb9eab8e06f2accea8
rajveer ray <rai314075@gmail.com>
2026-05-02 22:13:45 +0530

    Implement API fetching with async/await
    Added async function to fetch cat facts from API and display on button click.

--- java script/appfetchingAPI.js
+++ java script/appfetchingAPI.js
@@ -1,1 +1,30 @@
+////////////////////////////This is a example of fatching api in with async await ///////////////////////////////////
+//const URL ="https://cat-fact.herokuapp.com/facts"
+ 
+/*const getfacts = async() =>{///////here i use  async await with fetching ////////
+    console.log("getting Data ....");
+let promise = await fetch(URL);
+console.log(promise.status);//////Data return here in JSON format 
+}
+ getfacts()*/
 
+///////////////How to convert J S O N data into object DATA;/////////////////////////////////////////////////////
+
+///////Here we connecting the HTML button with this api//////////////////////////////////
+
+const URL ="https://cat-fact.herokuapp.com/facts"
+
+const btn = document.querySelector("#btn");
+const factpara = document.querySelector("#fact")
+
+
+btn.addEventListener("click", (async () => {//////Here we connect button withs click listener//////////////////////////////////////
+        console.log("getting Data ....");
+        let response = await fetch(URL);
+        console.log(response.status); //////Data return here in JSON format   After that we need to convert data into javascript object////////
+        let data = await response.json(); /////Here we use second await with json method in async await //////////////////////////////////////////
+
+        //////////console.log(Data[2])/NOT important///////////
+        factpara.innerText = data[1].text;
+
+    }));
